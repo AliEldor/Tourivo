d6585ded28fb29f13d1fe5743dfcd2e7575fa837
Ali Eldor <alieldor02@gmail.com>
2025-05-02 00:04:28 +0300

    Tour  model done

--- backend/models/Tour.js
+++ backend/models/Tour.js
@@ -23,7 +23,26 @@ const tourSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    }
+    desc: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: true,
+    },
+
+
+    featured: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
 );
 
 export default mongoose.model("Tour", tourSchema);
