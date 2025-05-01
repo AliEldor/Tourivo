254bc8e569509f52634ee8e586d4f6ec76a4c7bd
Ali Eldor <alieldor02@gmail.com>
2025-05-01 23:59:03 +0300

    create Tour  model

---
+++ backend/models/Tour.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const tourSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    distance: {
+      type: Number,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    }
+);
+
+export default mongoose.model("Tour", tourSchema);
