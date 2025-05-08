36e05b4f1ae167a4a4e75778abcbef04514336eb
Ali Eldor <alieldor02@gmail.com>
2025-05-08 18:46:26 +0300

    add updateTour method

--- backend/services/TourService.js
+++ backend/services/TourService.js
@@ -1,20 +1,46 @@
-import Tour from "../models/Tour.js"
+import Tour from "../models/Tour.js";
 
 export const TourService = {
-    createTour: async (tourData) => {
-        try {
-          const newTour = new Tour(tourData);
-          const savedTour = await newTour.save();
-          return {
-            success: true,
-            data: savedTour
-          };
-        } catch (err) {
-          return {
-            success: false,
-            error: "Failed to create tour"
-          };
-        }
-      },
+  createTour: async (tourData) => {
+    try {
+      const newTour = new Tour(tourData);
+      const savedTour = await newTour.save();
+      return {
+        success: true,
+        data: savedTour,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        error: "Failed to create tour",
+      };
+    }
+  },
 
-}
+  updateTour: async (id, tourData) => {
+    try {
+      const updatedTour = await Tour.findByIdAndUpdate(
+        id,
+        { $set: tourData },
+        { new: true }
+      );
+
+      if (!updatedTour) {
+        return {
+          success: false,
+          error: "Tour not found",
+        };
+      }
+
+      return {
+        success: true,
+        data: updatedTour,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        error: "Failed to update tour",
+      };
+    }
+  },
+};
