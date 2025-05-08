f391051397c154bf5b4af60b0c7866ecab33fc0c
Ali Eldor <alieldor02@gmail.com>
2025-05-08 19:26:43 +0300

    add getAllTours  method

--- backend/services/TourService.js
+++ backend/services/TourService.js
@@ -47,22 +47,22 @@ export const TourService = {
   deleteTour: async (id) => {
     try {
       const deletedTour = await Tour.findByIdAndDelete(id);
-      
+
       if (!deletedTour) {
         return {
           success: false,
-          error: "Tour not found"
+          error: "Tour not found",
         };
       }
-      
+
       return {
         success: true,
-        message: "Successfully deleted"
+        message: "Successfully deleted",
       };
     } catch (err) {
       return {
         success: false,
-        error: "Failed to delete tour"
+        error: "Failed to delete tour",
       };
     }
   },
@@ -70,11 +70,45 @@ export const TourService = {
   getSingleTour: async (id) => {
     try {
       const tour = await Tour.findById(id).populate("reviews");
-      
+
       if (!tour) {
         return {
           success: false,
-          error: "Tour not found"
+          error: "Tour not found",
         };
       }
+
+      return {
+        success: true,
+        data: tour,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        error: "Failed to find tour",
+      };
+    }
+  },
+
+
+  getAllTours: async (page = 0) => {
+    try {
+      const tours = await Tour.find({})
+        .populate("reviews")
+        .skip(page * 8)
+        .limit(8);
+      
+      return {
+        success: true,
+        count: tours.length,
+        data: tours
+      };
+    } catch (err) {
+      return {
+        success: false,
+        error: "Failed to find tours"
+      };
+    }
+  },
+
 };
