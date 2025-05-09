ba0945aaaa53fab1d5b475c6e2328dcd919678e3
Ali Eldor <alieldor02@gmail.com>
2025-05-09 03:33:32 +0300

    add getSingleTourController

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -38,3 +38,15 @@ export const deleteTour = async (req, res) => {
     return ResponseTrait.successResponse(res, { message: "Successfully deleted" });
   };
 
+  // Get single tour
+export const getSingleTour = async (req, res) => {
+    const { id } = req.params;
+    const response = await TourService.getSingleTour(id);
+    
+    if (!response.success) {
+      return ResponseTrait.errorResponse(res, response.error, response.error === "Tour not found" ? 404 : 500);
+    }
+    
+    return ResponseTrait.successResponse(res, response.data);
+  };
+
