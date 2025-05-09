91f18bc8d0d7b7c3b68d2e3ae7d440cc7bd20425
Ali Eldor <alieldor02@gmail.com>
2025-05-09 03:09:54 +0300

    add updateTourController

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -13,3 +13,15 @@ export const createTour = async (req, res) => {
       return ResponseTrait.successResponse(res, response.data);
     };
 
+    // Update tour
+export const updateTour = async (req, res) => {
+    const { id } = req.params;
+    const response = await TourService.updateTour(id, req.body);
+    
+    if (!response.success) {
+      return ResponseTrait.errorResponse(res, response.error, response.error === "Tour not found" ? 404 : 500);
+    }
+    
+    return ResponseTrait.successResponse(res, response.data);
+  };
+
