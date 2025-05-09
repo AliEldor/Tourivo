6f74c4aff41999fde739cdff5429d6a80efa7b58
Ali Eldor <alieldor02@gmail.com>
2025-05-09 03:07:52 +0300

    add createTourController

--- backend/controllers/tourController.js
+++ backend/controllers/tourController.js
@@ -1,6 +1,15 @@
 import { TourService } from "../services/TourService.js";
 import { ResponseTrait } from "../traits/ResponseTrait.js";
 
+
+// Create new tour
 export const createTour = async (req, res) => {
-    
-}
+    const response = await TourService.createTour(req.body);
+
+    if (!response.success) {
+        return ResponseTrait.errorResponse(res, response.error, 500);
+      }
+      
+      return ResponseTrait.successResponse(res, response.data);
+    };
+
