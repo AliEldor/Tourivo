51360780a032bab612e6861b2ffb258113d087f7
Ali Eldor <alieldor02@gmail.com>
2025-05-10 02:37:29 +0300

    add error handling to createUser method

--- backend/services/UserService.js
+++ backend/services/UserService.js
@@ -1,14 +1,19 @@
 import User from "../models/User.js";
 
 export const UserService = {
-    createUser: async (userData) => {
-        try {
-        const newUser = new User(userData);
-        const savedUser = await newUser.save();
-        return {
-            success: true,
-            data: savedUser,
-          };
-        }
+  createUser: async (userData) => {
+    try {
+      const newUser = new User(userData);
+      const savedUser = await newUser.save();
+      return {
+        success: true,
+        data: savedUser,
+      };
+    } catch (err) {
+      return {
+        success: false,
+        error: "Failed to create user",
+      };
     }
-}
+  },
+};
