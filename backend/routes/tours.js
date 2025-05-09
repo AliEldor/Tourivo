096cf4ce3a97c7acaa884eca42448b49b710904c
Ali Eldor <alieldor02@gmail.com>
2025-05-09 03:50:08 +0300

    initialize tours routes

---
+++ backend/routes/tours.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  createTour,
+  deleteTour,
+  getAllTour,
+  getFeaturedTour,
+  getSingleTour,
+  getTourBySearch,
+  getTourCount,
+  updateTour,
+} from "../controllers/tourController.js";
+import { validate, validateTour } from "../requests/TourRequest.js";
