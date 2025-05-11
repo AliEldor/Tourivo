10a78bcad505f3c02d4350743b6c4c2c29117c50
Ali Eldor <alieldor02@gmail.com>
2025-05-11 23:48:34 +0300

    add update user controller method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -11,3 +11,19 @@ export const createUser = async (req, res) => {
   
     return ResponseTrait.successResponse(res, response.data);
   };
+
+  // Update user
+export const updateUser = async (req, res) => {
+    const { id } = req.params;
+    const response = await UserService.updateUser(id, req.body);
+  
+    if (!response.success) {
+      return ResponseTrait.errorResponse(
+        res,
+        response.error,
+        response.error === "User not found" ? 404 : 500
+      );
+    }
+  
+    return ResponseTrait.successResponse(res, response.data);
+  };
