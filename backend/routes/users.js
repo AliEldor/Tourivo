7430ccb5fac4264bc41842c26988d893c1e1862e
Ali Eldor <alieldor02@gmail.com>
2025-05-12 02:16:05 +0300

    add update user route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,3 +10,6 @@ import { validate, validateUser } from "../requests/UserRequest.js";
 import { verifyAdmin, verifyUser } from "../utils/verifyToken.js";
 
 const router = express.Router();
+
+// Update user
+router.put("/:id", verifyUser, validateUser("updateUser"), validate, updateUser);
