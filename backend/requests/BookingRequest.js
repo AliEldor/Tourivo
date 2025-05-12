19a3bc8b1c6507e4641905f3befe178d59e144f4
Ali Eldor <alieldor02@gmail.com>
2025-05-12 17:38:08 +0300

    add phone number validation to createBooking

--- backend/requests/BookingRequest.js
+++ backend/requests/BookingRequest.js
@@ -3,15 +3,22 @@ import { ResponseTrait } from "../traits/ResponseTrait.js";
 import { validationResult } from "express-validator";
 
 export const validateBooking = (method) => {
-    switch (method) {
-        case "createBooking": {
-            return [
-                body("tourName").notEmpty().withMessage("Tour name is required"),
-                body("fullName").notEmpty().withMessage("Full name is required"),
-                body("guestSize")
-          .notEmpty().withMessage("Guest size is required")
-          .isInt({ min: 1 }).withMessage("Guest size must be at least 1"),
-            ] 
-        }
+  switch (method) {
+    case "createBooking": {
+      return [
+        body("tourName").notEmpty().withMessage("Tour name is required"),
+        body("fullName").notEmpty().withMessage("Full name is required"),
+        body("guestSize")
+          .notEmpty()
+          .withMessage("Guest size is required")
+          .isInt({ min: 1 })
+          .withMessage("Guest size must be at least 1"),
+        body("phone")
+          .notEmpty()
+          .withMessage("Phone number is required")
+          .isNumeric()
+          .withMessage("Phone must be a number"),
+      ];
     }
-}
+  }
+};
