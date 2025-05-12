89d53d301f07b2fc3f99e57de1bc71566d6458f8
Ali Eldor <alieldor02@gmail.com>
2025-05-12 04:16:32 +0300

    add verifyUser middleware

--- backend/utils/verifyToken.js
+++ backend/utils/verifyToken.js
@@ -27,3 +27,16 @@ export const verifyToken = (req, res, next) => {
   });
 };
 
+export const verifyUser = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user.id === req.params.id || req.user.role === "admin") {
+        next();
+      } else {
+        return ResponseTrait.errorResponse(
+          res, 
+          "You're not authenticated", 
+          401
+        );
+      }
+    });
+  };
