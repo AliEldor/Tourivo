18bbc406003cef4ba8376d4fbde67c1d8e644735
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:45:05 +0300

    add userId to default booking

--- backend/factories/BookingFactory.js
+++ backend/factories/BookingFactory.js
@@ -3,7 +3,7 @@ import faker from 'faker';
 const BookingFactory = {
     create: async (userId, tourName, overrides = {}) => {
         const defaultBooking = {
-            
+            userId: userId,
         }
     }
 }
