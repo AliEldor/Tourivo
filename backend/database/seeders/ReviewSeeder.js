280df77de98b0532de55547ce549e24b5efa1a43
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:51:34 +0300

    add ReviewSeeder with seed and truncate methods

--- backend/database/seeders/ReviewSeeder.js
+++ backend/database/seeders/ReviewSeeder.js
@@ -1,20 +1,22 @@
-import Tour from '../../models/Tour.js';
-import Review from '../../models/Review.js';
-import { ReviewFactory } from '../../factories/index.js';
+import Tour from "../../models/Tour.js";
+import Review from "../../models/Review.js";
+import { ReviewFactory } from "../../factories/index.js";
 
 const ReviewSeeder = {
-    seed: async (tours, users, reviewsPerTour = 3) => {
-        const reviews = [];
+  seed: async (tours, users, reviewsPerTour = 3) => {
+    const reviews = [];
 
-        for (const tour of tours) {
-            // Pick random users to create reviews
-      const selectedUsers = users.sort(() => 0.5 - Math.random()).slice(0, reviewsPerTour);
-      
+    for (const tour of tours) {
+      // Pick random users to create reviews
+      const selectedUsers = users
+        .sort(() => 0.5 - Math.random())
+        .slice(0, reviewsPerTour);
+
       for (const user of selectedUsers) {
         const reviewData = await ReviewFactory.create(tour._id, user._id, {
-            username: user.username
-          });
-          const review = new Review(reviewData);
+          username: user.username,
+        });
+        const review = new Review(reviewData);
         await review.save();
         reviews.push(review);
 
@@ -22,7 +24,14 @@ const ReviewSeeder = {
         tour.reviews.push(review._id);
         await tour.save();
       }
-
-        }
     }
-}
+    console.log(`${reviews.length} reviews seeded`);
+    return reviews;
+  },
+  truncate: async () => {
+    await Review.deleteMany({});
+    console.log("Reviews collection cleared");
+  },
+};
+
+export default ReviewSeeder;
