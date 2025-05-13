953fee03547268d50989cc1ea45aee05898ec52b
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:21:51 +0300

    add UserFactory for seeding database

--- backend/factories/UserFactory.js
+++ backend/factories/UserFactory.js
@@ -1,2 +1,14 @@
 import faker from 'faker';
 import bcrypt from 'bcryptjs';
+
+const UserFactory = {
+    create: async (overrides = {}) => {
+      const defaultUser = {
+        username: faker.internet.userName(),
+        email: faker.internet.email(),
+        password: await bcrypt.hash('password123', 10),
+        photo: faker.image.avatar(),
+        role: 'user'
+      };
+    }
+}
