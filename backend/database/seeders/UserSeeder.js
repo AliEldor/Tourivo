cc9fabfde3a607b893b4a116a3ddc670b42aa347
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:50:27 +0300

    add admin user to UserSeeder

--- backend/database/seeders/UserSeeder.js
+++ backend/database/seeders/UserSeeder.js
@@ -4,5 +4,12 @@ import { UserFactory } from '../../factories/index.js';
 const UserSeeder = {
     seed: async (count = 10) => {
         const users = [];
+
+        const adminData = await UserFactory.create({ 
+            username: 'admin',
+            email: 'admin@example.com',
+            role: 'admin' 
+          });
+
     }
 }
