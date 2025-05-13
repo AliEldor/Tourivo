d30bc5a6d0e8c1caf623b0a462d761ff11f2debf
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:43:01 +0300

    add return value to create method in ReviewFactory

--- backend/factories/ReviewFactory.js
+++ backend/factories/ReviewFactory.js
@@ -1,13 +1,17 @@
-import faker from 'faker';
+import faker from "faker";
 
 const ReviewFactory = {
-    create: async (tourId, userId, overrides = {}) => {
-        const defaultReview = {
-            productId: tourId,
-            userId: userId,
-            username: faker.internet.userName(),
-            reviewText: faker.lorem.paragraph(),
-            rating: faker.datatype.number({ min: 1, max: 5 })
-        }
-    }
-}
+  create: async (tourId, userId, overrides = {}) => {
+    const defaultReview = {
+      productId: tourId,
+      userId: userId,
+      username: faker.internet.userName(),
+      reviewText: faker.lorem.paragraph(),
+      rating: faker.datatype.number({ min: 1, max: 5 }),
+    };
+
+    return { ...defaultReview, ...overrides };
+  },
+};
+
+export default ReviewFactory;
