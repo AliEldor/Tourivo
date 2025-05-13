f28d23cdb0d0abb84cf73f5a6e0245104c53e358
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:01:03 +0300

    seed tours using Tour model save method

--- backend/database/seeders/TourSeeder.js
+++ backend/database/seeders/TourSeeder.js
@@ -1,14 +1,20 @@
-import Tour from '../../models/Tour.js';
-import { TourFactory } from '../../factories/index.js';
+import Tour from "../../models/Tour.js";
+import { TourFactory } from "../../factories/index.js";
 
 const TourSeeder = {
-    seed: async (count = 20) => {
-        const tours =[]
+  seed: async (count = 20) => {
+    const tours = [];
 
-        for (let i = 0; i < count; i++) {
-            const tourData = await TourFactory.create({
-              featured: i < 8 // make the first 8 tours featured
-            });
-        }
+    for (let i = 0; i < count; i++) {
+      const tourData = await TourFactory.create({
+        featured: i < 8, // make the first 8 tours featured
+      });
+      const tour = new Tour(tourData);
+      await tour.save();
+      tours.push(tour);
     }
-}
+
+    console.log(`${tours.length} tours seeded`);
+    return tours;
+  },
+};
