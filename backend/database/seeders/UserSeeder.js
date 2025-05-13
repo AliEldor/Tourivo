2aeaf5133b24355dafb61f913a90ab7056e76b61
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:51:10 +0300

    seed admin user in UserSeeder

--- backend/database/seeders/UserSeeder.js
+++ backend/database/seeders/UserSeeder.js
@@ -1,15 +1,17 @@
-import User from '../../models/User.js';
-import { UserFactory } from '../../factories/index.js';
+import User from "../../models/User.js";
+import { UserFactory } from "../../factories/index.js";
 
 const UserSeeder = {
-    seed: async (count = 10) => {
-        const users = [];
+  seed: async (count = 10) => {
+    const users = [];
 
-        const adminData = await UserFactory.create({ 
-            username: 'admin',
-            email: 'admin@example.com',
-            role: 'admin' 
-          });
-
-    }
-}
+    const adminData = await UserFactory.create({
+      username: "admin",
+      email: "admin@example.com",
+      role: "admin",
+    });
+    const admin = new User(adminData);
+    await admin.save();
+    users.push(admin);
+  },
+};
