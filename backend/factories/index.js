b5723c56f7b000a2d2aded459e90706e468a77ad
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:47:45 +0300

    add index file for factories

---
+++ backend/factories/index.js
@@ -0,0 +1,11 @@
+import UserFactory from './UserFactory.js';
+import TourFactory from './TourFactory.js';
+import ReviewFactory from './ReviewFactory.js';
+import BookingFactory from './BookingFactory.js';
+
+export {
+  UserFactory,
+  TourFactory,
+  ReviewFactory,
+  BookingFactory
+};
