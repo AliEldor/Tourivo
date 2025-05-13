0a7e98af3594ac8e342b6942206ae54e6efd660c
Ali Eldor <alieldor02@gmail.com>
2025-05-13 13:45:08 +0300

    add rating validation

--- backend/requests/ReviewRequest.js
+++ backend/requests/ReviewRequest.js
@@ -3,14 +3,18 @@ import { ResponseTrait } from "../traits/ResponseTrait.js";
 import { validationResult } from "express-validator";
 
 export const validateReview = (method) => {
-    switch (method) {
-        case "createReview": {
-            return [
-                param("tourId").isMongoId().withMessage("Invalid tour ID"),
-                body("username").notEmpty().withMessage("Username is required"),
-                body("reviewText").notEmpty().withMessage("Review text is required"),
-                
-            ]
-        }
+  switch (method) {
+    case "createReview": {
+      return [
+        param("tourId").isMongoId().withMessage("Invalid tour ID"),
+        body("username").notEmpty().withMessage("Username is required"),
+        body("reviewText").notEmpty().withMessage("Review text is required"),
+        body("rating")
+          .notEmpty()
+          .withMessage("Rating is required")
+          .isInt({ min: 0, max: 5 })
+          .withMessage("Rating must be between 0 and 5"),
+      ];
     }
-}
+  }
+};
