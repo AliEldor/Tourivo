d7c874d3cca4a30f9d7c3b0971cd1a61758682cf
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:27:17 +0300

    add featured and reviews fields to TourFactory

--- backend/factories/TourFactory.js
+++ backend/factories/TourFactory.js
@@ -1,18 +1,22 @@
-import faker from 'faker';
+import faker from "faker";
 
 const TourFactory = {
-    create: async (overrides = {}) => {
-        const defaultTour = {
-            title: `${faker.address.city()} Adventure Tour`,
-            city: faker.address.city(),
-            address: faker.address.streetAddress(),
-            distance: faker.datatype.number({ min: 10, max: 1000 }),
-            photo: faker.image.nature(),
-            desc: faker.lorem.paragraphs(3),
-            price: faker.datatype.number({ min: 50, max: 2000 }),
-            maxGroupSize: faker.datatype.number({ min: 1, max: 20 }),
-        }
-    }
-}
+  create: async (overrides = {}) => {
+    const defaultTour = {
+      title: `${faker.address.city()} Adventure Tour`,
+      city: faker.address.city(),
+      address: faker.address.streetAddress(),
+      distance: faker.datatype.number({ min: 10, max: 1000 }),
+      photo: faker.image.nature(),
+      desc: faker.lorem.paragraphs(3),
+      price: faker.datatype.number({ min: 50, max: 2000 }),
+      maxGroupSize: faker.datatype.number({ min: 1, max: 20 }),
+      featured: faker.datatype.boolean(),
+      reviews: [],
+    };
+
+    return { ...defaultTour, ...overrides };
+  },
+};
 
 export default TourFactory;
