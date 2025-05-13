302d38cf98130bc4fbc0b25064088cb25f45ee94
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:55:30 +0300

    add booking seeder

---
+++ backend/database/seeders/BookingSeeder.js
@@ -0,0 +1,8 @@
+import Booking from '../../models/Booking.js';
+import { BookingFactory } from '../../factories/index.js';
+
+const BookingSeeder = {
+    seed: async (tours, users, bookingsCount = 30) => {
+        const bookings = [];
+    }
+}
