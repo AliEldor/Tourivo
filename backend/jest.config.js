fe4611c0683cc4be8e8789c30e179c70524f1575
Ali Eldor <alieldor02@gmail.com>
2025-05-13 23:04:12 +0300

    update jest config to run feature tests

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,10 +1,17 @@
 export default {
     testEnvironment: 'node',
-    testMatch: ['**/tests/**/*.js'],
+    testMatch: ['**/tests/features/**/*.js'], 
     setupFilesAfterEnv: ['./tests/setup.js'],
     verbose: true,
     forceExit: true,
     clearMocks: true,
     resetMocks: true,
-    restoreMocks: true
+    restoreMocks: true,
+    testTimeout: 30000,
+    detectOpenHandles: true,
+    transform: {},
+    moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1', 
+  },
+
   };
