03d0941db74bb6967c90d44b902ff3b8c210b4e8
Ali Eldor <alieldor02@gmail.com>
2025-05-13 21:19:43 +0300

    add database seeder index file

---
+++ backend/database/seeders/index.js
@@ -0,0 +1,10 @@
+import UserSeeder from './UserSeeder.js';
+import TourSeeder from './TourSeeder.js';
+import ReviewSeeder from './ReviewSeeder.js';
+import BookingSeeder from './BookingSeeder.js';
+
+const DatabaseSeeder = {
+    seedAll: async () => {
+        
+    }
+}
