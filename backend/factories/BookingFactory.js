4a5b44af688f55e84401814e3814f1b8d051edad
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:46:49 +0300

    add phone and bookAt fields to BookingFactory

--- backend/factories/BookingFactory.js
+++ backend/factories/BookingFactory.js
@@ -1,15 +1,17 @@
-import faker from 'faker';
+import faker from "faker";
 
 const BookingFactory = {
-    create: async (userId, tourName, overrides = {}) => {
-        const defaultBooking = {
-            userId: userId,
-            userEmail: faker.internet.email(),
-            tourName: tourName,
-            fullName: faker.name.findName(),
-            guestSize: faker.datatype.number({ min: 1, max: 8 }),
-        }
-    }
-}
+  create: async (userId, tourName, overrides = {}) => {
+    const defaultBooking = {
+      userId: userId,
+      userEmail: faker.internet.email(),
+      tourName: tourName,
+      fullName: faker.name.findName(),
+      guestSize: faker.datatype.number({ min: 1, max: 8 }),
+      phone: parseInt(faker.phone.phoneNumberFormat().replace(/\D/g, "")),
+      bookAt: faker.date.future(),
+    };
+  },
+};
 
 export default BookingFactory;
