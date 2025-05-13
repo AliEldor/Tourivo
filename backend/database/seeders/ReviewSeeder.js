4d778fbff2eeb3dd4a0569626a43eca8f00d0e28
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:49:58 +0300

    add review seeder logic

--- backend/database/seeders/ReviewSeeder.js
+++ backend/database/seeders/ReviewSeeder.js
@@ -5,5 +5,17 @@ import { ReviewFactory } from '../../factories/index.js';
 const ReviewSeeder = {
     seed: async (tours, users, reviewsPerTour = 3) => {
         const reviews = [];
+
+        for (const tour of tours) {
+            // Pick random users to create reviews
+      const selectedUsers = users.sort(() => 0.5 - Math.random()).slice(0, reviewsPerTour);
+      
+      for (const user of selectedUsers) {
+        const reviewData = await ReviewFactory.create(tour._id, user._id, {
+            username: user.username
+          });
+      }
+
+        }
     }
 }
