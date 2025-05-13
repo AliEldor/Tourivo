029542d3e0f7f3c74f858cad80917e3ee31b337e
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:41:26 +0300

    add productId to review factory

--- backend/factories/ReviewFactory.js
+++ backend/factories/ReviewFactory.js
@@ -3,7 +3,7 @@ import faker from 'faker';
 const ReviewFactory = {
     create: async (tourId, userId, overrides = {}) => {
         const defaultReview = {
-            
+            productId: tourId,
         }
     }
 }
