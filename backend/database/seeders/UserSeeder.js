9e8f8a9c013680a4c57f130e5509352f8f86e798
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:49:54 +0300

    add user seeder file

---
+++ backend/database/seeders/UserSeeder.js
@@ -0,0 +1,8 @@
+import User from '../../models/User.js';
+import { UserFactory } from '../../factories/index.js';
+
+const UserSeeder = {
+    seed: async (count = 10) => {
+        const users = [];
+    }
+}
