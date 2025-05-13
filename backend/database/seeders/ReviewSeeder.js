b9b371c07fd7ea09c9f7f5fe6be4a19a4096b9cd
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:49:02 +0300

    add ReviewSeeder file

---
+++ backend/database/seeders/ReviewSeeder.js
@@ -0,0 +1,9 @@
+import Tour from '../../models/Tour.js';
+import Review from '../../models/Review.js';
+import { ReviewFactory } from '../../factories/index.js';
+
+const ReviewSeeder = {
+    seed: async (tours, users, reviewsPerTour = 3) => {
+        const reviews = [];
+    }
+}
