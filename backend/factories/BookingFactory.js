9f23ed81d30af41656aed9683e8e7a8732fbbb8e
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:44:41 +0300

    add BookingFactory file

---
+++ backend/factories/BookingFactory.js
@@ -0,0 +1,11 @@
+import faker from 'faker';
+
+const BookingFactory = {
+    create: async (userId, tourName, overrides = {}) => {
+        const defaultBooking = {
+            
+        }
+    }
+}
+
+export default BookingFactory;
