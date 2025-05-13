2fce831e7dc49f8774d9fd56a65f1d2a763e374f
Ali Eldor <alieldor02@gmail.com>
2025-05-13 19:22:22 +0300

    add return statement

--- backend/factories/UserFactory.js
+++ backend/factories/UserFactory.js
@@ -1,14 +1,17 @@
-import faker from 'faker';
-import bcrypt from 'bcryptjs';
+import faker from "faker";
+import bcrypt from "bcryptjs";
 
 const UserFactory = {
-    create: async (overrides = {}) => {
-      const defaultUser = {
-        username: faker.internet.userName(),
-        email: faker.internet.email(),
-        password: await bcrypt.hash('password123', 10),
-        photo: faker.image.avatar(),
-        role: 'user'
-      };
-    }
-}
+  create: async (overrides = {}) => {
+    const defaultUser = {
+      username: faker.internet.userName(),
+      email: faker.internet.email(),
+      password: await bcrypt.hash("password123", 10),
+      photo: faker.image.avatar(),
+      role: "user",
+    };
+    return { ...defaultUser, ...overrides };
+  },
+};
+
+export default UserFactory;
