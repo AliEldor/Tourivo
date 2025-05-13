e98d09530655cc90c453874fcce39056c168b55b
Ali Eldor <alieldor02@gmail.com>
2025-05-13 23:05:03 +0300

    update tour factory with faker-js and unsplash image url

--- backend/factories/TourFactory.js
+++ backend/factories/TourFactory.js
@@ -1,16 +1,16 @@
-import faker from "faker";
+import { faker } from '@faker-js/faker';
 
 const TourFactory = {
   create: async (overrides = {}) => {
     const defaultTour = {
-      title: `${faker.address.city()} Adventure Tour`,
-      city: faker.address.city(),
-      address: faker.address.streetAddress(),
-      distance: faker.datatype.number({ min: 10, max: 1000 }),
-      photo: faker.image.nature(),
+      title: `${faker.location.city()} Adventure Tour`,
+      city: faker.location.city(),
+      address: faker.location.streetAddress(),
+      distance: faker.number.int({ min: 10, max: 1000 }),
+      photo: `https://source.unsplash.com/random/800x600/?travel,${faker.location.city().toLowerCase()}`,
       desc: faker.lorem.paragraphs(3),
-      price: faker.datatype.number({ min: 50, max: 2000 }),
-      maxGroupSize: faker.datatype.number({ min: 1, max: 20 }),
+      price: faker.number.int({ min: 50, max: 2000 }),
+      maxGroupSize: faker.number.int({ min: 1, max: 20 }),
       featured: faker.datatype.boolean(),
       reviews: [],
     };
