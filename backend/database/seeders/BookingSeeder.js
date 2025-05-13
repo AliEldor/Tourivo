3bab2397a86dabe1c396041204728d72d1d02de6
Ali Eldor <alieldor02@gmail.com>
2025-05-13 20:56:04 +0300

    add booking seeder logic

--- backend/database/seeders/BookingSeeder.js
+++ backend/database/seeders/BookingSeeder.js
@@ -4,5 +4,11 @@ import { BookingFactory } from '../../factories/index.js';
 const BookingSeeder = {
     seed: async (tours, users, bookingsCount = 30) => {
         const bookings = [];
+
+        for (let i = 0; i < bookingsCount; i++) {
+            const randomUser = users[Math.floor(Math.random() * users.length)];
+      const randomTour = tours[Math.floor(Math.random() * tours.length)];
+        }
+
     }
 }
