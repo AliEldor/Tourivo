fd75070bb4194af2f345c9e07b48907bc9a41ac2
Ali Eldor <alieldor02@gmail.com>
2025-05-14 03:47:47 +0300

    add validateGeneratedTrip method

--- backend/requests/GeneratedTripRequest.js
+++ backend/requests/GeneratedTripRequest.js
@@ -1,3 +1,14 @@
 import { body, param } from "express-validator";
 import { ResponseTrait } from "../traits/ResponseTrait.js";
 import { validationResult } from "express-validator";
+
+export const validateGeneratedTrip = (method) => {
+    switch (method) {
+        case "generateTrip": {
+            return [
+
+            ];
+        }
+
+    }
+}
