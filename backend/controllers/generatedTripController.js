35eb3076e99dda5ae95dbb1a9b992f3588f6a915
Ali Eldor <alieldor02@gmail.com>
2025-05-14 03:38:05 +0300

    add generateTrip endpoint

--- backend/controllers/generatedTripController.js
+++ backend/controllers/generatedTripController.js
@@ -1,5 +1,20 @@
 import { GeneratedTripService } from "../services/GeneratedTripService.js";
 import { ResponseTrait } from "../traits/ResponseTrait.js";
 
+export const generateTrip = async (req, res) => {
+  const userId = req.user.id;
+  const preferences = req.body;
+
+  const response = await GeneratedTripService.generateTrip(userId, preferences);
+
+  if (!response.success) {
+    return ResponseTrait.errorResponse(res, response.error, 500);
+  }
+
+  return ResponseTrait.successResponse(res, {
+    message: "Trip generated successfully",
+    data: response.data,
+  });
+};
 
 
