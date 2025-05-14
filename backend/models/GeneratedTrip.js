3265eb4551b7651c28b1f8832360190cbac5bc27
Ali Eldor <alieldor02@gmail.com>
2025-05-14 03:07:49 +0300

    initialize GeneratedTrip model

---
+++ backend/models/GeneratedTrip.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const generatedTripSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+  }
+)
