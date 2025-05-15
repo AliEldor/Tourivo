cb7a04d466605f91161cd73c898c65f6789ec1cb
Ali Eldor <alieldor02@gmail.com>
2025-05-15 19:13:37 +0300

    add validatePhotoDetection method

--- backend/requests/PhotoDetection.js
+++ backend/requests/PhotoDetection.js
@@ -2,3 +2,30 @@ import { body, param } from "express-validator";
 import { ResponseTrait } from "../traits/ResponseTrait.js";
 import { validationResult } from "express-validator";
 
+export const validatePhotoDetection = (method) => {
+  switch (method) {
+    case "uploadPhoto": {
+      return [
+        body("tourId")
+          .optional()
+          .isMongoId()
+          .withMessage("Invalid tour ID"),
+        body("tripId")
+          .optional()
+          .isMongoId()
+          .withMessage("Invalid trip ID"),
+        body("description")
+          .optional()
+          .isString()
+          .withMessage("Description must be a string"),
+        body("isPublic")
+          .optional()
+          .isBoolean()
+          .withMessage("isPublic must be a boolean value"),
+      ];
+    }
+
+    
+  }
+};
+
