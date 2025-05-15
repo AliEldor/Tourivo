87a3adfffc9d8abe3e422945e76825fc1c781a18
Ali Eldor <alieldor02@gmail.com>
2025-05-15 18:36:21 +0300

    add PhotoDetectionService

---
+++ backend/services/PhotoDetectionService.js
@@ -0,0 +1,8 @@
+import PhotoDetection from "../models/PhotoDetection.js";
+import fs from "fs";
+import path from "path";
+import vision from "@google-cloud/vision";
+import dotenv from "dotenv";
+
+dotenv.config();
+
