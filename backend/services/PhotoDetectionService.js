08d2c4f1427e060bfa64c4c536602912f51581bc
Ali Eldor <alieldor02@gmail.com>
2025-05-15 18:36:43 +0300

    initialize vision client

--- backend/services/PhotoDetectionService.js
+++ backend/services/PhotoDetectionService.js
@@ -6,3 +6,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
+// Vision client 
+let client;
+try {
+  const keyFilePath = path.resolve(process.env.GOOGLE_CLOUD_CREDENTIALS);
+  console.log("Looking for credentials file at:", keyFilePath);
+  
+  client = new vision.ImageAnnotatorClient({
+    keyFilename: keyFilePath
+  });
+  
+  console.log("Vision client initialized successfully!");
+} catch (error) {
+  console.error("Failed to initialize Vision client:", error);
+}
+
