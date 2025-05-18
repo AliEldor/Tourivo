f5311fb212dd9f4cd8cc8d9bcf19599cb1333f70
Ali Eldor <alieldor02@gmail.com>
2025-05-18 06:17:22 +0300

    initialize MasonryImagesGallery component

---
+++ frontend/src/components/Image-gallery/MasonryImagesGallery.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+import Masonry,{ResponsiveMasonry} from 'react-responsive-masonry'
+import galleryImages from './galleryImages'
+
+
+
+export default MasonryImagesGallery
