39e7befc88ed295ef09502422b0212bcd939f20d
Ali Eldor <alieldor02@gmail.com>
2025-05-18 17:11:53 +0300

    add TourCard component structure

--- frontend/src/shared/TourCard.jsx
+++ frontend/src/shared/TourCard.jsx
@@ -5,6 +5,12 @@ import calculateAvgRating from "../utils/avgRating";
 
 import "./tour-card.css";
 
+const TourCard = ({ tour }) => {
+  const { _id, title, city, photo, price, featured, reviews } = tour;
 
+  const { totalRating, avgRating } = calculateAvgRating(reviews);
+
+  
+};
 
 export default TourCard;
