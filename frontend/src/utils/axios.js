d17bc3c1cf04ef6fad1f576e041ee9381b1ec286
Ali Eldor <alieldor02@gmail.com>
2025-05-18 03:59:45 +0300

    add axios interceptors for request and response handling

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -10,6 +10,29 @@ const axiosInstance = axios.create({
 });
 
 
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      
+      if (error.response.status === 401) {
+        
+        console.log("Unauthorized access");
+      }
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
