a7c8b5764788c45047a157cbae93cdae151e0d8b
Ali Eldor <alieldor02@gmail.com>
2025-05-18 06:03:52 +0300

    add FeaturedTourList component

---
+++ frontend/src/components/Featured-tours/FeaturedTourList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import TourCard from "../../shared/TourCard";
+import { Col } from "reactstrap";
+import useAxios from "../../hooks/useAxios";
+
+
+
+export default FeaturedTourList;
