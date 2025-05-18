7d6ec8205ea814b9349e6ce503594b29f330ad31
Ali Eldor <alieldor02@gmail.com>
2025-05-18 05:42:02 +0300

    add header component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useRef, useContext } from "react";
+import { Container, Row, Button } from "reactstrap";
+import { NavLink, Link, useNavigate, useLocation } from "react-router-dom";
+
+import logo from "../../assets/images/Tourivo2.png";
+import "./header.css";
+import { AuthContext } from "./../../context/AuthContext.jsx";
+
+
+
+export default Header;
