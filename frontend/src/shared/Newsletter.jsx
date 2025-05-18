a2730de6a6161d64d83c7a9c363b05faef7ce39c
Ali Eldor <alieldor02@gmail.com>
2025-05-18 16:50:25 +0300

    initialize newsletter component

---
+++ frontend/src/shared/Newsletter.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./newsletter.css";
+
+import { Container, Row, Col } from "reactstrap";
+import maleTourist from "../assets/images/male-tourist.png";
+
+
+
+export default Newsletter;
