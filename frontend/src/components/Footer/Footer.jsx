673ad014a0e538ad4d026aa7fcafd4caf747235d
Ali Eldor <alieldor02@gmail.com>
2025-05-18 05:59:21 +0300

    add quick links data to footer

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,6 +4,21 @@ import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
 import { Link } from "react-router-dom";
 import logo from "../../assets/images/Tourivo logo.png";
 
+const quick__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
 
 
 export default Footer;
