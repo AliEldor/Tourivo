1e339e63d7a5d981e0cab36422bfbdef622813b1
Ali Eldor <alieldor02@gmail.com>
2025-05-18 17:06:01 +0300

    add searchbar component

---
+++ frontend/src/shared/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React, { useRef } from "react";
+import "./search-bar.css";
+import { Col, Form, FormGroup } from "reactstrap";
+
+import axiosInstance from "../utils/axios";
+
+import { useNavigate } from "react-router-dom";
+
+
+
+export default SearchBar;
