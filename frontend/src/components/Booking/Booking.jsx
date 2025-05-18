f5cb13812c83b2d3f70d6d2f0701a276ed91f7e8
Ali Eldor <alieldor02@gmail.com>
2025-05-18 06:05:59 +0300

    add getUserEmail method to Booking component

--- frontend/src/components/Booking/Booking.jsx
+++ frontend/src/components/Booking/Booking.jsx
@@ -5,6 +5,15 @@ import { useNavigate, Link } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext.jsx";
 import axiosInstance from "../../utils/axios";
 
+const Booking = ({ tour, avgRating }) => {
+  const { price, reviews, title } = tour;
+  const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
+  const getUserEmail = () => {
+    return user?.data?.data?.email;
+  };
+
+};
 
 export default Booking;
