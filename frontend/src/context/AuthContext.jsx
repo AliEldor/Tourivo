16fd88d05774958203cb43ff31c4dcf3fd91af49
Ali Eldor <alieldor02@gmail.com>
2025-05-18 05:39:48 +0300

    add AuthReducer to manage authentication state

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -11,7 +11,43 @@ const initial_state = {
 
 export const AuthContext = createContext(initial_state);
 
+const AuthReducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        loading: true,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "REGISTER_SUCCESS":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
 
+    default:
+      return state;
+  }
+};
 
 export const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AuthReducer, initial_state);
