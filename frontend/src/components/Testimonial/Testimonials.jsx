d07d8b1acb553586cf6596d95077b8d0c277cffb
Ali Eldor <alieldor02@gmail.com>
2025-05-18 06:19:58 +0300

    add new testimonial slide

--- frontend/src/components/Testimonial/Testimonials.jsx
+++ frontend/src/components/Testimonial/Testimonials.jsx
@@ -63,6 +63,19 @@ const Testimonial = () => {
           </div>
          </div>
 
+         <div className='testimonial py-4 px-3'>
+      <p>in theory you should use redux. </p>
+
+           <div className='d-flex align-items-center gap-4 mt-3'>
+           <img src={ava03} className='w-25 h-25 rounded-2' alt='' />
+           <div>
+            <h6 className='mb-0 mt-3'>Taha Taha</h6>
+            <p>Customer</p>
+           </div>
+          </div>
+         </div>
+
+
          
   </Slider>
   );
