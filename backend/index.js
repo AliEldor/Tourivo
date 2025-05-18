ca33f22aac3c9e06febfa407a1fd9a391f3a4ab1
Ali Eldor <alieldor02@gmail.com>
2025-05-18 03:38:04 +0300

    update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,26 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import cookieParser from 'cookie-parser';
-import dns from 'dns'; 
-
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import dns from "dns";
+import path from "path";
+import { fileURLToPath } from "url";
 
 // import routes
-import tourRoute from './routes/tours.js';
-import userRoute from './routes/users.js';
-import authRoute from './routes/auth.js';
-import bookingRoute from './routes/bookings.js';
-import reviewRoute from './routes/reviews.js';
-import generatedTripRoute from './routes/generatedTrips.js';
-import photoDetectionRoute  from './routes/photoDetections.js';
+import tourRoute from "./routes/tours.js";
+import userRoute from "./routes/users.js";
+import authRoute from "./routes/auth.js";
+import bookingRoute from "./routes/bookings.js";
+import reviewRoute from "./routes/reviews.js";
+import generatedTripRoute from "./routes/generatedTrips.js";
+import photoDetectionRoute from "./routes/photoDetections.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //  DNS setting for connection issue
-dns.setServers(['8.8.8.8', '8.8.4.4']);
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 dotenv.config();
 const app = express();
@@ -25,44 +28,47 @@ const port = process.env.PORT || 8000;
 
 // Database connection
 const connect = async () => {
-    try {
-        
-        await mongoose.connect(process.env.MONGO_URI, {
-            serverSelectionTimeoutMS: 15000, 
-            family: 4 // ipv4
-        });
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      serverSelectionTimeoutMS: 15000,
+      family: 4, // ipv4
+    });
 
-        console.log('MongoDB database connected');
-    } 
-    catch (err) {
-        console.log('MongoDB database connection failed');
-        console.log('Error details:', err.message);
-    }
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log("MongoDB database connection failed");
+    console.log("Error details:", err.message);
+  }
 };
 
-// For testing 
-app.get('/', (req, res) => {
-    res.send("api is working");
+// For testing
+app.get("/", (req, res) => {
+  res.send("api is working");
 });
 
 // Middleware
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     origin: true,
-    credentials: true
-  }));
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 
-//routes
-app.use('/api/v1/auth', authRoute);
-app.use('/api/v1/tours', tourRoute);
-app.use('/api/v1/users', userRoute);
-app.use('/api/v1/booking', bookingRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use('/api/v1/generated-trips', generatedTripRoute);
-app.use('/api/v1/photo-detections', photoDetectionRoute);
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+console.log("Serving static files from:", path.join(__dirname, "uploads"));
 
-app.listen(port, () => { 
-    connect();
-    console.log("server listening on port", port);
+//routes
+app.use("/api/v1/auth", authRoute);
+app.use("/api/v1/tours", tourRoute);
+app.use("/api/v1/users", userRoute);
+app.use("/api/v1/booking", bookingRoute);
+app.use("/api/v1/reviews", reviewRoute);
+app.use("/api/v1/generated-trips", generatedTripRoute);
+app.use("/api/v1/photo-detections", photoDetectionRoute);
+
+app.listen(port, () => {
+  connect();
+  console.log("server listening on port", port);
 });
