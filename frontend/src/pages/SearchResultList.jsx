7b4b1bf2d192dc7ef5b62a8277cf75d63c54373b
Ali Eldor <alieldor02@gmail.com>
2025-05-20 06:46:50 +0300

    add SearchResultList component

--- frontend/src/pages/SearchResultList.jsx
+++ frontend/src/pages/SearchResultList.jsx
@@ -7,6 +7,12 @@ import { useLocation } from "react-router-dom";
 import TourCard from "../shared/TourCard";
 import Newsletter from './../shared/Newsletter'
 
+const SearchResultList = () => {
+  const location = useLocation();
 
+  const [data] = useState(location.state);
+
+  
+};
 
 export default SearchResultList;
