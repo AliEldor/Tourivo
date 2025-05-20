202274ad269cb2b8ffe73de66f5664b02682550f
Ali Eldor <alieldor02@gmail.com>
2025-05-20 13:02:21 +0300

    add initial Routers component

---
+++ frontend/src/router/Routers.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Tours from "../pages/Tours";
+import TourDetails from "../pages/TourDetails";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import SearchResultList from "../pages/SearchResultList";
+import ThankYou from "../pages/ThankYou";
+import About from "../pages/About";
+import GenerateTrip from "../pages/GenerateTrip";
+import TripResult from "../pages/TripResult";
+import PhotoDetection from "../pages/PhotoDetection";
+
+
+
+export default Routers;
