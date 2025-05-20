41e9be41270da40d9536a9119512ffdd0f6e789f
Ali Eldor <alieldor02@gmail.com>
2025-05-21 01:33:05 +0300

    add LoadingSpinner logic

--- frontend/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ frontend/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Spinner } from "reactstrap";
 import "./LoadingSpinner.css";
 
-
+const LoadingSpinner = () => {
+  return (
+    <div className="spinner-container">
+      <Spinner color="primary" />
+      <p>Loading...</p>
+    </div>
+  );
+};
 
 export default LoadingSpinner;
