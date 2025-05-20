ce116f8bcefc080d9a56e247496f58196d553894
Ali Eldor <alieldor02@gmail.com>
2025-05-20 12:58:22 +0300

    add basic tour details display

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -98,7 +98,43 @@ const TourDetails = () => {
         <Container>
           {loading && <h4 className="text-center pt-5">Loading....</h4>}
           {error && <h4 className="text-center pt-5">{error}</h4>}
-          
+          {!loading && !error && (
+            <Row>
+              <Col lg="8">
+                <div className="tour__content">
+                  <img src={photo} alt="" />
+
+                  <div className="tour__info">
+                    <h2>{title}</h2>
+                    <div className="d-flex align-items-center gap-5">
+                      <span className="tour__rating d-flex align-items-center gap-1">
+                        <i
+                          className="ri-star-fill"
+                          style={{ color: "var(--secondary-color)" }}
+                        ></i>
+                        {avgRating === 0 ? null : avgRating}
+                        {totalRating === 0 ? (
+                          "Not rated"
+                        ) : (
+                          <span>({reviews?.length})</span>
+                        )}
+                      </span>
+
+                      <span>
+                        <i className="ri-map-pin-user-fill"></i> {address}
+                      </span>
+                    </div>
+
+                    
+                  </div>
+
+                  
+                </div>
+              </Col>
+
+              
+            </Row>
+          )}
         </Container>
       </section>
       <Newsletter />
