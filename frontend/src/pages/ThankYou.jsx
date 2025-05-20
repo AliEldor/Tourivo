8ba2cf8b50aa1eeef086808a286559774372cce7
Ali Eldor <alieldor02@gmail.com>
2025-05-20 13:04:45 +0300

    add thank you page classes

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -7,7 +7,25 @@ const ThankYou = () => {
   const location = useLocation();
   const booking = location.state;
 
-  
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg="12" className="pt-5 text-center">
+            <div className="thank__you">
+              <span>
+                <i className="ri-checkbox-circle-line"></i>
+              </span>
+              <h1 className="mb-3 fw-semibold">Thank You</h1>
+              <h3 className="mb-4">Your tour is booked</h3>
+
+              
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default ThankYou;
