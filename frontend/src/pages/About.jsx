7c5f49f5c22e8b1810eaf2055a06bc0f38a80ba0
Ali Eldor <alieldor02@gmail.com>
2025-05-20 12:24:06 +0300

    initialize about page component

---
+++ frontend/src/pages/About.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import CommonSection from "../shared/CommonSection";
+
+import "../styles/tour.css";
+
+import SearchBar from "./../shared/SearchBar";
+import Newsletter from "./../shared/Newsletter";
+import { Col, Container, Row } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import worldImg from "../assets/images/world.png";
+import Subtitle from "../shared/Subtitle";
+
+
+
+export default About;
