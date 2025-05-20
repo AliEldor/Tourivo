714c11742423ba3f9f1323b8cc03d1f7c5bf626d
Ali Eldor <alieldor02@gmail.com>
2025-05-21 01:28:49 +0300

    add protected route component

--- frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -4,6 +4,18 @@ import { AuthContext } from "../../context/AuthContext";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 
 
+const ProtectedRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
 
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children ? children : <Outlet />;
+};
 
 export default ProtectedRoute;
