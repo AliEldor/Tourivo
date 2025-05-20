82099afacdff750f05bde9791b620122c92b678d
Ali Eldor <alieldor02@gmail.com>
2025-05-21 02:27:56 +0300

    initialize PhotoDetection page

---
+++ frontend/src/pages/PhotoDetection.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+import PhotoUpload from "../components/PhotoUpload/PhotoUpload";
+import PhotoResult from "../components/PhotoDetection/PhotoResult";
+import "../styles/photo-detection.css";
+import CommonSection from "../shared/CommonSection";
+
+const PhotoDetection = () => {
+  
+};
+
+export default PhotoDetection;
