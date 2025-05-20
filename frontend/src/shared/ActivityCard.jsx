4172d5575ed7f421eab90b17449f7f072735633a
Ali Eldor <alieldor02@gmail.com>
2025-05-21 02:32:48 +0300

    initialize ActivityCard

---
+++ frontend/src/shared/ActivityCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card, CardBody } from 'reactstrap';
+import { Link } from 'react-router-dom';
+import calculateAvgRating from '../utils/avgRating';
+
+import './tour-card.css';
+
+const ActivityCard = ({ activity }) => {
+  
+};
+
+export default ActivityCard;
