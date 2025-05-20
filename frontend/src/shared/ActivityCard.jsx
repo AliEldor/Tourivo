48ca4d512750cb09c9e3566d800cc5bddaf68fb2
Ali Eldor <alieldor02@gmail.com>
2025-05-21 02:33:50 +0300

    add ActivityCard component content

--- frontend/src/shared/ActivityCard.jsx
+++ frontend/src/shared/ActivityCard.jsx
@@ -18,7 +18,30 @@ const ActivityCard = ({ activity }) => {
           {featured && <span>Featured</span>}
         </div>
 
-        
+        <CardBody>
+          <div className='card__top d-flex align-items-center justify-content-between'>
+            <span className='activity__location d-flex align-items-center gap-1'>
+              <i className='ri-map-pin-line'></i> {city}
+            </span>
+
+            <span className='activity__rating d-flex align-items-center gap-1'>
+              <i className='ri-star-fill'></i> {avgRating === 0 ? null : avgRating}
+              {totalRating === 0 ? 'Not rated' : <span>({reviews.length})</span>}
+            </span>
+          </div>
+
+          <h5 className='activity__title'>
+            <Link to={`/activities/${id}`}>{title}</Link>
+          </h5>
+
+          <div className='card__bottom d-flex align-items-center justify-content-between mt-3'>
+            <h5>${price} <span>/per person</span></h5>
+
+            <button className='btn booking__btn'>
+              <Link to={`/activities/${id}`}>Book Now</Link>
+            </button>
+          </div>
+        </CardBody>
       </Card>
     </div>
   );
