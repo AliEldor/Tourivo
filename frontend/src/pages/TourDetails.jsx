82f62eabd651c88e14eb827a43b99db63e4a84e4
Ali Eldor <alieldor02@gmail.com>
2025-05-20 12:51:16 +0300

    initialize tour details page component

---
+++ frontend/src/pages/TourDetails.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useRef, useState, useContext } from "react";
+import "../styles/tour-details.css";
+import { Container, Row, Col, Form, ListGroup } from "reactstrap";
+import { useParams } from "react-router-dom";
+import calculateAvgRating from "../utils/avgRating";
+import avatar from "../assets/images/avatar.jpg";
+import Booking from "../components/Booking/Booking";
+import Newsletter from "../shared/Newsletter";
+import useAxios from "../hooks/useAxios";
+import { AuthContext } from "../context/AuthContext.jsx";
+import axiosInstance from "../utils/axios";
+
+
+
+export default TourDetails;
