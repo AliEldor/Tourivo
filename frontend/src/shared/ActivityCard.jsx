741225ef56aaf5c050a9b22448dd68cb0e2ab18d
Ali Eldor <alieldor02@gmail.com>
2025-05-21 02:33:16 +0300

    add activity card component structure

--- frontend/src/shared/ActivityCard.jsx
+++ frontend/src/shared/ActivityCard.jsx
@@ -6,7 +6,11 @@ import calculateAvgRating from '../utils/avgRating';
 import './tour-card.css';
 
 const ActivityCard = ({ activity }) => {
-  
+  const { id, title, city, photo, price, featured, reviews } = activity;
+
+  const { totalRating, avgRating } = calculateAvgRating(reviews);
+
+ 
 };
 
 export default ActivityCard;
