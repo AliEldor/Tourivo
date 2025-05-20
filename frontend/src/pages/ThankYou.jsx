225d380d9798c831623c21f828322927c318f944
Ali Eldor <alieldor02@gmail.com>
2025-05-20 13:03:41 +0300

    initialize ThankYou page component

---
+++ frontend/src/pages/ThankYou.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container, Row, Col, Button } from "reactstrap";
+import { Link, useLocation } from "react-router-dom";
+import "../styles/thank-you.css";
+
+
+
+export default ThankYou;
