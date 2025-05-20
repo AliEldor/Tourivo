d1b2ff49ce1d6c31866a6604487d91493b3feb7f
Ali Eldor <alieldor02@gmail.com>
2025-05-20 06:51:15 +0300

    initialize home page component

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/home.css";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import experienceImg from "../assets/images/experience.png";
+
+import Subtitle from "../shared/Subtitle";
+
+import SearchBar from "../shared/SearchBar";
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from "../components/Featured-tours/FeaturedTourList";
+import MasonryImagesGallery from "../components/Image-gallery/MasonryImagesGallery";
+import Testimonials from "../components/Testimonial/Testimonials";
+import Newsletter from "../shared/Newsletter";
+
+
+
+export default Home;
