f6bebc35ff99946788e34e00917ea2eddfaba3bb
Ali Eldor <alieldor02@gmail.com>
2025-05-20 12:44:05 +0300

    initialize tours page component

---
+++ frontend/src/pages/Tours.jsx
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from "react";
+import CommonSection from "../shared/CommonSection";
+
+import "../styles/tour.css";
+
+import TourCard from "./../shared/TourCard";
+import SearchBar from "./../shared/SearchBar";
+import Newsletter from "./../shared/Newsletter";
+import { Col, Container, Row } from "reactstrap";
+
+import useAxios from "../hooks/useAxios";
+
+
+
+export default Tour;
