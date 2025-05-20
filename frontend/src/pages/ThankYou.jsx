a2accb76e01ba10e66ff87d33d6d253215b82656
Ali Eldor <alieldor02@gmail.com>
2025-05-20 13:04:00 +0300

    add ThankYou component structure

--- frontend/src/pages/ThankYou.jsx
+++ frontend/src/pages/ThankYou.jsx
@@ -3,6 +3,11 @@ import { Container, Row, Col, Button } from "reactstrap";
 import { Link, useLocation } from "react-router-dom";
 import "../styles/thank-you.css";
 
+const ThankYou = () => {
+  const location = useLocation();
+  const booking = location.state;
 
+  
+};
 
 export default ThankYou;
