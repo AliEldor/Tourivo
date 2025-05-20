47511d3d003a8b7264967ca6b702e48479f0f889
Ali Eldor <alieldor02@gmail.com>
2025-05-20 06:12:39 +0300

    initialize login page component

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useContext } from "react";
+import { Container, Row, Col, Form, FormGroup, Button } from "reactstrap";
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/login.css";
+
+import loginImg from "../assets/images/login-img.png";
+
+import { AuthContext } from "./../context/AuthContext.jsx";
+import axiosInstance from "../utils/axios";
+
+
+
+export default Login;
