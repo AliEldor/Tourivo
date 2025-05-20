e8c88eb59acdcd7a030691e28d9e279d9d92d0d6
Ali Eldor <alieldor02@gmail.com>
2025-05-20 15:02:13 +0300

    add uploadPhoto method

--- frontend/src/services/photoService.js
+++ frontend/src/services/photoService.js
@@ -1,3 +1,16 @@
 import axiosInstance from "../utils/axios";
 
+export const uploadPhoto = async (formData) => {
+  try {
+    const response = await axiosInstance.post("/photo-detections", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw error.response?.data || error.message;
+  }
+};
+
 
