5545d53be9a350eba2ac51bc666561e3ef74536c
Ali Eldor <alieldor02@gmail.com>
2025-05-20 12:24:26 +0300

    add about page structure

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -10,6 +10,19 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import worldImg from "../assets/images/world.png";
 import Subtitle from "../shared/Subtitle";
 
+const About = () => {
+  return (
+    <>
+      <CommonSection title={"About"} />
+      <section>
+        <Container>
+          <Row></Row>
+        </Container>
+      </section>
 
+      
+    </>
+  );
+};
 
 export default About;
