b0ce633738dff17c913c2e9d71299b7d7b610d47
Ali Eldor <alieldor02@gmail.com>
2025-05-20 06:48:23 +0300

    add search Logic

--- frontend/src/pages/SearchResultList.jsx
+++ frontend/src/pages/SearchResultList.jsx
@@ -18,7 +18,15 @@ const SearchResultList = () => {
       <section>
         <Container>
           <Row>
-            
+            {data.length === 0 ? (
+              <h4 className="text-center">No tour found</h4>
+            ) : (
+              data?.map((tour) => (
+                <Col lg="3" className="mb-4" key={tour._id}>
+                  <TourCard tour={tour} />
+                </Col>
+              ))
+            )}
           </Row>
         </Container>
       </section>
