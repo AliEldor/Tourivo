7422853ad24dde3f7782ee016bc0cb82bfd9feda
Ali Eldor <alieldor02@gmail.com>
2025-05-21 08:48:07 +0300

    initialize ai trip generator prompt file

---
+++ backend/utils/aiTripGeneratorPrompt.js
@@ -0,0 +1,6 @@
+import { ChatOpenAI } from "@langchain/openai";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StructuredOutputParser } from "langchain/output_parsers";
+import { z } from "zod";
+
+
