7009d6e31f268484a28b04fc520c19209e351161
Ali Eldor <alieldor02@gmail.com>
2025-05-21 08:49:25 +0300

    add tripOutputParser with zod schema

--- backend/utils/aiTripGeneratorPrompt.js
+++ backend/utils/aiTripGeneratorPrompt.js
@@ -3,4 +3,11 @@ import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StructuredOutputParser } from "langchain/output_parsers";
 import { z } from "zod";
 
+// create output parser with Zod schema
+export const tripOutputParser = StructuredOutputParser.fromZodSchema(
+  z.object({
+    
+  })
+);
+
 
