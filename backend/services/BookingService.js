90b755a5b2b8d77a9eb1496669c5bdbeb998bd7d
Ali Eldor <alieldor02@gmail.com>
2025-05-21 07:31:26 +0300

    update getAllBookings method

--- backend/services/BookingService.js
+++ backend/services/BookingService.js
@@ -19,5 +19,21 @@ export const BookingService = {
     return booking;
   },
 
-  
+  getAllBookings: async () => {
+    const bookings = await Booking.find();
+
+    return {
+      count: bookings.length,
+      data: bookings,
+    };
+  },
+
+  getUserBookings: async (userId) => {
+    const bookings = await Booking.find({ userId: userId });
+
+    return {
+      count: bookings.length,
+      data: bookings,
+    };
+  },
 };
